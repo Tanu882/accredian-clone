17f5f6c1041249c683e0a71d0e0ca68e69503e94
Tanu Yadav <yadavtanu882@gmail.com>
2026-04-26 20:30:54 +0530

    installed framer motion library for Animation

--- app/page.js
+++ app/page.js
@@ -2,6 +2,8 @@ import Partners from "../components/Partners";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Stats from "../components/Stats";
+import Edge from "../components/Edge";
+
 
 
 
@@ -12,6 +14,7 @@ export default function Home() {
       <Hero />
       <Stats />
       <Partners />
+      <Edge />
     </div>
   );
 }
